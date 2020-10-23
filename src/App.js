0429590efebc3b63f7b553cb588f800294096b77
khanhafe <khanhafe@gmail.com>
2020-10-23 12:13:33 +0530

    dynamic data dynamic data with event handler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 import Mydata from "./Mydata";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
+class App extends Component {
+  state = {
+    person: [
+      { name: "Hafeez", age: "29" },
+      { name: "Mujju", age: "29" },
+      { name: "Afshan", age: "24" }
+    ]
+  };
 
-      <Mydata />
-      <Mydata />
-      <Mydata />
-    </div>
-  );
+  switchNameHandler = (myParameter) => {
+    this.setState({
+      person: [
+        { name: myParameter, age: "29" },
+        { name: "Mujju", age: "30" },
+        { name: "Afshan", age: "24" }
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+
+        <Mydata
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+        />
+        <Mydata
+          click={this.switchNameHandler.bind(this, "Khan")}
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+        >
+          He is very special
+        </Mydata>
+        <Mydata
+          name={this.state.person[2].name}
+          age={this.state.person[2].age}
+        />
+        <button onClick={() => this.switchNameHandler("arfaaz")}>
+          Try me!
+        </button>
+      </div>
+    );
+  }
 }
+export default App;
--- src/Mydata.js
+++ src/Mydata.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-const mydata = () => {
-  return <h3>Hello functional component</h3>;
+const mydata = (props) => {
+  return (
+    <p onClick={props.click}>
+      Hello my name is {props.name} and my Age is {props.age}.
+      <span style={{ color: "red" }}> {props.children}</span>
+    </p>
+  );
 };
 export default mydata;
