e42830e42a14158354e89e6a8932e7b35b74ef8b
khanhafe <khanhafe@gmail.com>
2020-11-17 16:15:30 +0530

    form validation form  validation final

--- src/Validationform.js
+++ src/Validationform.js
@@ -1,19 +1,72 @@
 import React, { Component } from "react";
 
 class Validationform extends Component {
-  validateForm = () => {
-    console.log("form button clicked");
-    return false;
+  state = {
+    formData: {},
+    errors: {}
   };
+  handleOnChange = (event) => {
+    let formData = this.state.formData;
+    formData[event.target.name] = event.target.value;
+
+    this.setState({
+      formData
+    });
+    console.log(formData);
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.validateForm()) {
+      let formData = {};
+      formData["name"] = "";
+      formData["email"] = "";
+      formData["comment"] = "";
+      this.setState({ formData: formData });
+
+      alert("Demo Form is submited");
+    }
+  };
+
+  validateForm = () => {
+    let forenteredData = this.state.formData;
+    let errors = {};
+    let isValid = true;
+    if (!forenteredData["name"]) {
+      isValid = false;
+      errors["name"] = "please enter name";
+    }
+    if (!forenteredData["email"]) {
+      isValid = false;
+      errors["email"] = "please enter Email";
+    }
+    if (!forenteredData["password"]) {
+      isValid = false;
+      errors["password"] = "please enter password";
+    }
+    this.setState({
+      errors: errors
+    });
+    return isValid;
+  };
+
   render() {
     return (
       <div>
-        <form onSubmit={this.validateForm}>
-          Name :<input type="text" name="name" />
+        <form onSubmit={this.handleSubmit}>
+          Name :<input type="text" name="name" onChange={this.handleOnChange} />
+          {this.state.errors["name"]}
           <br />
-          Email :<input type="text" name="email" />
+          Email :
+          <input type="text" name="email" onChange={this.handleOnChange} />
+          {this.state.errors.email}
           <br />
-          Password :<input type="password" name="name" />
+          Password :
+          <input
+            type="password"
+            name="password"
+            onChange={this.handleOnChange}
+          />
+          {this.state.errors.password}
           <br />
           <input type="submit" />
         </form>
