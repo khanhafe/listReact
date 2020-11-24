0a22baaf376431ac28485083b871009497b3f8e7
khanhafe <khanhafe@gmail.com>
2020-11-24 11:51:56 +0530

    counter counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    counter: 0
+  };
+  changeCounter = (action, value) => {
+    switch (action) {
+      case "inc":
+        this.setState((prevState) => {
+          return { counter: prevState.counter + 1 };
+        });
+        break;
+      case "dec":
+        this.setState((prevState) => {
+          return { counter: prevState.counter - 1 };
+        });
+        break;
+      case "add":
+        this.setState((prevState) => {
+          return { counter: prevState.counter + 5 };
+        });
+        break;
+      case "sub":
+        this.setState((prevState) => {
+          return { counter: prevState.counter - 5 };
+        });
+        break;
+      default:
+        return;
+    }
+  };
   render() {
-    return <div>this is my counter component</div>;
+    return (
+      <div
+        style={{ border: "4px solid red", marginTop: "20px", height: "200px" }}
+      >
+        <div>my Result :{this.state.counter} </div>
+        <br />
+        <button onClick={() => this.changeCounter("inc")}>Increment</button>
+        <button onClick={() => this.changeCounter("dec")}>Decrement</button>
+        <button onClick={() => this.changeCounter("add", 5)}>Add</button>
+        <button onClick={() => this.changeCounter("sub", 5)}>Subtract</button>
+      </div>
+    );
   }
 }
 
